cbf6c82d4f965b7effdf4b68eeb78995e0ea8159
Christian <us3l3ss1@hotmail.com>
2019-07-30 15:56:23 -0700

    Updates

---
+++ precache-manifest.eec84892296a1866b487146825d8da9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd527ecb4647e9ccf5cfe979e943a6f",
+    "url": "/Face-Detect/index.html"
+  },
+  {
+    "revision": "6f3e25249f9d031b78c5",
+    "url": "/Face-Detect/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "61a9ed026d08051c47d9",
+    "url": "/Face-Detect/static/css/main.6ff829fa.chunk.css"
+  },
+  {
+    "revision": "6f3e25249f9d031b78c5",
+    "url": "/Face-Detect/static/js/2.2ae84360.chunk.js"
+  },
+  {
+    "revision": "61a9ed026d08051c47d9",
+    "url": "/Face-Detect/static/js/main.939cd718.chunk.js"
+  },
+  {
+    "revision": "aa3b440f74f4c7e70f8e",
+    "url": "/Face-Detect/static/js/runtime~main.490e080b.js"
+  }
+]);
